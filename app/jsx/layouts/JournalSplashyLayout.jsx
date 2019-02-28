67bb97708b7654b96fec5854d1d1b132bc901c4d
Andy Mardesich <andy.mardesich@ucop.edu>
2019-02-28 11:22:10 -0800

    Items in 'magazine layout' should not break across columns

--- app/jsx/layouts/JournalSplashyLayout.jsx
+++ app/jsx/layouts/JournalSplashyLayout.jsx
@@ -74,7 +74,21 @@ class JournalSplashyLayout extends React.Component {
               <ItemActionsComp />
               <CustomSelectorObj />
               <IssueComp />
-              <h3 className="o-heading1a">Table of Contents</h3>
+              <h3 className="o-heading1a">Introduction</h3>
+              <div className="o-dividecontent2x--ruled">
+                <div className="c-pub">
+                  <h4 className="c-pub__heading"><a href="">Italy in the Mediterranean Today: A New Critical Topography</a></h4>
+                  <div className="c-authorlist">
+                    <ul className="c-authorlist__list">
+                      <li><a href="">Fogu, Claudio</a></li>
+                      <li><a href="">Re, Lucia</a></li>
+                      <li><a href="" className="c-authorlist__list-more-link">et al.</a></li>
+                    </ul>
+                  </div>
+                  <div className="c-pub__abstract"><div className="c-clientmarkup"><p>Editors' Introduction to Volume 1, Issue 1.</p></div></div>
+                </div>
+              </div>
+              <h3 className="o-heading1a">A Critical Map of Italy in the Mediterranean - Defining the terms</h3>
               <div className="o-dividecontent2x--ruled">
                 <PubComp />
                 <img src="http://placehold.it/300x150?text=Image" alt="" />

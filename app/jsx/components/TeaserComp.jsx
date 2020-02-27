c33d7fb47b8988e5abfaa1be041ee0c1b07a09a3
JoelUCOP <joel.hagedorn@ucop.edu>
2020-02-27 12:10:20 -0800

    swap repository section with publishing in home teaser

--- app/jsx/components/TeaserComp.jsx
+++ app/jsx/components/TeaserComp.jsx
@@ -8,19 +8,19 @@ class TeaserComp extends React.Component {
       <div className="c-teaser">
         <section>
           <header>
-            <h2>eScholarship Repository</h2>
-          </header>
-          <p>OA repository services for UC researchers to share their scholarly output.
-          </p>
-          <a href="#repository">Learn more</a>
-        </section>
-        <section>
-          <header>
             <h2>eScholarship Publishing</h2>
           </header>
           <p>OA publishing for journals, monographs, conference proceedings and more.
           </p>
           <a href="#publishing">Learn more</a>
+        </section>
+        <section>
+          <header>
+            <h2>eScholarship Repository</h2>
+          </header>
+          <p>OA repository services for UC researchers to share their scholarly output.
+          </p>
+          <a href="#repository">Learn more</a>
         </section>
       </div>
     )

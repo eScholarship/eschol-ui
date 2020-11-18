40819ff7127c22e5156a690bbc5c835a4cd8a8e5
JoelCDL <joel.hagedorn@ucop.edu>
2020-11-18 14:38:40 -0800

    clean up and clarify subheader comp current examples

--- app/jsx/display/SubheaderDisp.jsx
+++ app/jsx/display/SubheaderDisp.jsx
@@ -8,18 +8,10 @@ class SubheaderDisp extends React.Component {
   render() {
     return (
       <div>
-        <h3>For wide setting, Ideal banner dimensions are 9.19 x 1 and no higher than 87 pixels. NB: There is a restriction built into the CMS that should prevent images with incompatible dimensions from being submitted in the first place.</h3>
-        <hr />
-        <h2>Wide Setting: Logo image contains the full, legible title of your site. Banner = 675 x 75 pixels</h2>
+        <h2>Subheader Using Wide Banner</h2>
         <SubheaderComp banner={MEDIA_PATH + 'temp_journal-banner.png'} wide={true} />
-        <h2>Narrow Setting and Banner = 600 x 200 pixels</h2>
-        <SubheaderComp banner={"https://picsum.photos/600/200/?image=951"} wide={false} />
-        <h2>Wide Setting and Banner = 600 x 200 pixels (stretched horizontally)</h2>
-        <img src="https://via.placeholder.com/600x200?text=UC+San+Diego+(600x200)"/>
-        <SubheaderComp banner="https://via.placeholder.com/600x200?text=UC+San+Diego+(600x200)" wide={true} />
-        <h2>Wide Setting and Banner = 1100 x 87 pixels (too narrow)</h2>
-        <img src="https://via.placeholder.com/1100x87?text=UC+San+Diego+(1100x87)"/>
-        <SubheaderComp banner="https://via.placeholder.com/1100x87?text=UC+San+Diego+(1100x87)" wide={true} />
+        <h2>Subheader Using Narrow Banner</h2>
+        <SubheaderComp banner={'https://escholarship.org/assets/ebadc9294e27ce4df7a7391de182d062a094d4a0ff94c8fd7ca8b212fcd85541'} wide={false} />
       </div>
     )
   }

28f81c7affad76b2dfd256c7307b8c0b89494b5f
JoelCDL <joel.hagedorn@ucop.edu>
2020-12-10 14:05:54 -0800

    add campus label, separate title and banner in subheader comp

--- app/jsx/components/SubheaderComp.jsx
+++ app/jsx/components/SubheaderComp.jsx
@@ -5,12 +5,16 @@ import React from 'react'
 class SubheaderComp extends React.Component {
   render() {
     return (
-      <div className="c-subheader">
-        <a className={this.props.isWide ? "c-subheader__banner--wide" : "c-subheader__banner--narrow"} href={this.props.bannerLink}>
+      <div className={`c-subheader ${this.props.bannerUrl ? "has-banner" : ""} ${this.props.isWide ? "is-wide" : ""} ${this.props.campusLabel ? "has-campus-label" : ""}`}>
+        <a className="c-subheader__title" href={this.props.bannerLink}>
           <h1>{this.props.unitTitle}</h1>
-          {this.props.bannerUrl && <img src={this.props.bannerUrl} alt={this.props.unitTitle + ' banner'} /> }
         </a>
-        <div className="c-subheader__sidebar">
+        {this.props.bannerUrl && <a className="c-subheader__banner" href={this.props.bannerLink}>
+          <img src={this.props.bannerUrl} alt={this.props.unitTitle + ' banner'} />
+        </a> }
+        {this.props.campusLabel && <a className="c-subheader__campus" href={this.props.campusLink}>{this.props.campusLabel}
+        </a> }
+        <div className="c-subheader__buttons">
           <button className="o-button__3">{this.props.isDept ? "Deposit" : "Submit"}</button>
           <button className="o-button__3">Manage <span className="c-subheader__button-fragment">Submissions</span></button>
         </div>

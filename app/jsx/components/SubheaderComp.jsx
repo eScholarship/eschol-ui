91831fbd7a69b6181d18da469b7f3091b4091266
JoelCDL <joel.hagedorn@ucop.edu>
2020-12-16 12:14:15 -0800

    redefine subheader comp/disp as functions instead of classes

--- app/jsx/components/SubheaderComp.jsx
+++ app/jsx/components/SubheaderComp.jsx
@@ -2,25 +2,23 @@
 
 import React from 'react'
 
-class SubheaderComp extends React.Component {
-  render() {
-    return (
-      <div className={`c-subheader ${this.props.bannerUrl ? "has-banner" : ""} ${this.props.isWide ? "is-wide" : ""} ${this.props.campusLabel ? "has-campus-label" : ""}`}>
-        <a className="c-subheader__title" href={this.props.bannerLink}>
-          <h1>{this.props.unitTitle}</h1>
-        </a>
-        {this.props.bannerUrl && <a className="c-subheader__banner" href={this.props.bannerLink}>
-          <img src={this.props.bannerUrl} alt={this.props.unitTitle + ' banner'} />
-        </a> }
-        {this.props.campusLabel && <a className="c-subheader__campus" href={this.props.campusLink}>{this.props.campusLabel}
-        </a> }
-        <div className="c-subheader__buttons">
-          <button className="o-button__3">{this.props.isDept ? "Deposit" : "Submit"}</button>
-          <button className="o-button__3">Manage <span className="c-subheader__button-fragment">Submissions</span></button>
-        </div>
+function SubheaderComp(props) {
+  return (
+    <div className={`c-subheader ${props.bannerUrl ? "has-banner" : ""} ${props.isWide ? "is-wide" : ""} ${props.campusLabel ? "has-campus-label" : ""}`}>
+      <a className="c-subheader__title" href={props.bannerLink}>
+        <h1>{props.unitTitle}</h1>
+      </a>
+      {props.bannerUrl && <a className="c-subheader__banner" href={props.bannerLink}>
+        <img src={props.bannerUrl} alt={props.unitTitle + ' banner'} />
+      </a> }
+      {props.campusLabel && <a className="c-subheader__campus" href={props.campusLink}>{props.campusLabel}
+      </a> }
+      <div className="c-subheader__buttons">
+        <button className="o-button__3">{props.isDept ? "Deposit" : "Submit"}</button>
+        <button className="o-button__3">Manage <span className="c-subheader__button-fragment">Submissions</span></button>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 module.exports = SubheaderComp;
--- app/jsx/display/SubheaderDisp.jsx
+++ app/jsx/display/SubheaderDisp.jsx
@@ -4,37 +4,35 @@ import React from 'react'
 import SubheaderComp from '../components/SubheaderComp.jsx'
 import MEDIA_PATH from '../../js/MediaPath.js'
 
-class SubheaderDisp extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>Journal Subheader Using Wide Banner</h2>
-        <SubheaderComp bannerLink={'https://escholarship.org/uc/uciem_westjem'} unitTitle={'Western Journal of Emergency Medicine'} bannerUrl={MEDIA_PATH + 'temp_journal-banner.png'} isWide={true} />
-        <h2>Journal Subheader Using Wide Banner With Campus Label</h2>
-        <SubheaderComp bannerLink={'https://escholarship.org/uc/uciem_westjem'} unitTitle={'Western Journal of Emergency Medicine'} bannerUrl={MEDIA_PATH + 'temp_journal-banner.png'} isWide={true} campusLabel={'UC Irvine'} campusLink={'https://escholarship.org/uc/uci'} />
-        <h2>Journal Subheader Using Small Logo + Typeset Title</h2>
-        <SubheaderComp bannerLink={'https://escholarship.org/uc/ucbclassics_bujc'} unitTitle={'Berkeley Undergraduate Journal of Classics'} bannerUrl={'https://escholarship.org/assets/c6efb9a478093370f05eb67c08b132b74570d848571548c3a320c213183ff1dd'} isWide={false} />
-        <h2>Journal Subheader Using Small Logo + Typeset Title With Campus Label</h2>
-        <SubheaderComp bannerLink={'https://escholarship.org/uc/ucbclassics_bujc'} unitTitle={'Berkeley Undergraduate Journal of Classics'} bannerUrl={'https://escholarship.org/assets/c6efb9a478093370f05eb67c08b132b74570d848571548c3a320c213183ff1dd'} isWide={false} campusLabel={'UC Berkeley'} campusLink={'https://escholarship.org/uc/ucb'} />
-        <h2>Journal Subheader Using Typeset Title Only</h2>
-        <SubheaderComp bannerLink={'https://escholarship.org/uc/bling_formal_linguistics'} unitTitle={'Berkeley Papers in Formal Linguistics'} isWide={false} />
-        <h2>Journal Subheader Using Typeset Title Only With Campus Label</h2>
-        <SubheaderComp bannerLink={'https://escholarship.org/uc/bling_formal_linguistics'} unitTitle={'Berkeley Papers in Formal Linguistics'} isWide={false} campusLabel={'UC Berkeley'} campusLink={'https://escholarship.org/uc/ucb'} />
-        <h2>Department Subheader Using Wide Banner</h2>
-        <SubheaderComp bannerLink={'https://escholarship.org/uc/arf'} unitTitle={'Archaeological Research Facility'} isDept={true} bannerUrl={'https://escholarship.org/assets/f5bae7cdd66fe7d61508583785f181d93795ad7170e3fa573537ef06d51f4eb2'} isWide={true} />
-        <h2>Department Subheader Using Wide Banner With Campus Label</h2>
-        <SubheaderComp bannerLink={'https://escholarship.org/uc/arf'} unitTitle={'Archaeological Research Facility'} isDept={true} bannerUrl={'https://escholarship.org/assets/f5bae7cdd66fe7d61508583785f181d93795ad7170e3fa573537ef06d51f4eb2'} isWide={true}  campusLabel={'UC Berkeley'} campusLink={'https://escholarship.org/uc/ucb'} />
-        <h2>Department Subheader Using Small Logo + Typeset Title</h2>
-        <SubheaderComp bannerLink={'https://escholarship.org/uc/jmie_ice'} unitTitle={'Information Center for the Environment'} isDept={true} bannerUrl={'https://escholarship.org/assets/ebadc9294e27ce4df7a7391de182d062a094d4a0ff94c8fd7ca8b212fcd85541'} isWide={false} />
-        <h2>Department Subheader Using Small Logo + Typeset Title With Campus Label</h2>
-        <SubheaderComp bannerLink={'https://escholarship.org/uc/jmie_ice'} unitTitle={'Information Center for the Environment'} isDept={true} bannerUrl={'https://escholarship.org/assets/ebadc9294e27ce4df7a7391de182d062a094d4a0ff94c8fd7ca8b212fcd85541'} isWide={false} campusLabel={'UC Davis'} campusLink={'https://escholarship.org/uc/ucd'} />
-        <h2>Department Subheader Using Typeset Title Only</h2>
-        <SubheaderComp bannerLink={'https://escholarship.org/uc/big'} unitTitle={'Bay Area International Group'} isDept={true} isWide={false} />
-        <h2>Department Subheader Using Typeset Title Only With Campus Label</h2>
-        <SubheaderComp bannerLink={'https://escholarship.org/uc/big'} unitTitle={'Bay Area International Group'} isDept={true} isWide={false} campusLabel={'UC Berkeley'} campusLink={'https://escholarship.org/uc/ucb'} />
-      </div>
-    )
-  }
+function SubheaderDisp () {
+  return (
+    <div>
+      <h2>Journal Subheader Using Wide Banner</h2>
+      <SubheaderComp bannerLink={'https://escholarship.org/uc/uciem_westjem'} unitTitle={'Western Journal of Emergency Medicine'} bannerUrl={MEDIA_PATH + 'temp_journal-banner.png'} isWide={true} />
+      <h2>Journal Subheader Using Wide Banner With Campus Label</h2>
+      <SubheaderComp bannerLink={'https://escholarship.org/uc/uciem_westjem'} unitTitle={'Western Journal of Emergency Medicine'} bannerUrl={MEDIA_PATH + 'temp_journal-banner.png'} isWide={true} campusLabel={'UC Irvine'} campusLink={'https://escholarship.org/uc/uci'} />
+      <h2>Journal Subheader Using Small Logo + Typeset Title</h2>
+      <SubheaderComp bannerLink={'https://escholarship.org/uc/ucbclassics_bujc'} unitTitle={'Berkeley Undergraduate Journal of Classics'} bannerUrl={'https://escholarship.org/assets/c6efb9a478093370f05eb67c08b132b74570d848571548c3a320c213183ff1dd'} isWide={false} />
+      <h2>Journal Subheader Using Small Logo + Typeset Title With Campus Label</h2>
+      <SubheaderComp bannerLink={'https://escholarship.org/uc/ucbclassics_bujc'} unitTitle={'Berkeley Undergraduate Journal of Classics'} bannerUrl={'https://escholarship.org/assets/c6efb9a478093370f05eb67c08b132b74570d848571548c3a320c213183ff1dd'} isWide={false} campusLabel={'UC Berkeley'} campusLink={'https://escholarship.org/uc/ucb'} />
+      <h2>Journal Subheader Using Typeset Title Only</h2>
+      <SubheaderComp bannerLink={'https://escholarship.org/uc/bling_formal_linguistics'} unitTitle={'Berkeley Papers in Formal Linguistics'} isWide={false} />
+      <h2>Journal Subheader Using Typeset Title Only With Campus Label</h2>
+      <SubheaderComp bannerLink={'https://escholarship.org/uc/bling_formal_linguistics'} unitTitle={'Berkeley Papers in Formal Linguistics'} isWide={false} campusLabel={'UC Berkeley'} campusLink={'https://escholarship.org/uc/ucb'} />
+      <h2>Department Subheader Using Wide Banner</h2>
+      <SubheaderComp bannerLink={'https://escholarship.org/uc/arf'} unitTitle={'Archaeological Research Facility'} isDept={true} bannerUrl={'https://escholarship.org/assets/f5bae7cdd66fe7d61508583785f181d93795ad7170e3fa573537ef06d51f4eb2'} isWide={true} />
+      <h2>Department Subheader Using Wide Banner With Campus Label</h2>
+      <SubheaderComp bannerLink={'https://escholarship.org/uc/arf'} unitTitle={'Archaeological Research Facility'} isDept={true} bannerUrl={'https://escholarship.org/assets/f5bae7cdd66fe7d61508583785f181d93795ad7170e3fa573537ef06d51f4eb2'} isWide={true}  campusLabel={'UC Berkeley'} campusLink={'https://escholarship.org/uc/ucb'} />
+      <h2>Department Subheader Using Small Logo + Typeset Title</h2>
+      <SubheaderComp bannerLink={'https://escholarship.org/uc/jmie_ice'} unitTitle={'Information Center for the Environment'} isDept={true} bannerUrl={'https://escholarship.org/assets/ebadc9294e27ce4df7a7391de182d062a094d4a0ff94c8fd7ca8b212fcd85541'} isWide={false} />
+      <h2>Department Subheader Using Small Logo + Typeset Title With Campus Label</h2>
+      <SubheaderComp bannerLink={'https://escholarship.org/uc/jmie_ice'} unitTitle={'Information Center for the Environment'} isDept={true} bannerUrl={'https://escholarship.org/assets/ebadc9294e27ce4df7a7391de182d062a094d4a0ff94c8fd7ca8b212fcd85541'} isWide={false} campusLabel={'UC Davis'} campusLink={'https://escholarship.org/uc/ucd'} />
+      <h2>Department Subheader Using Typeset Title Only</h2>
+      <SubheaderComp bannerLink={'https://escholarship.org/uc/big'} unitTitle={'Bay Area International Group'} isDept={true} isWide={false} />
+      <h2>Department Subheader Using Typeset Title Only With Campus Label</h2>
+      <SubheaderComp bannerLink={'https://escholarship.org/uc/big'} unitTitle={'Bay Area International Group'} isDept={true} isWide={false} campusLabel={'UC Berkeley'} campusLink={'https://escholarship.org/uc/ucb'} />
+    </div>
+  )
 }
 
 module.exports = SubheaderDisp;

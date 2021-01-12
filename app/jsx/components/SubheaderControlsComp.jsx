c453c01adea948b2a268aaa5ca84f446c143c112
JoelCDL <joel.hagedorn@ucop.edu>
2021-01-11 17:17:02 -0800

    add color contrast comp within subheader controls comp

--- app/jsx/components/SubheaderControlsComp.jsx
+++ app/jsx/components/SubheaderControlsComp.jsx
@@ -3,17 +3,20 @@
 import React, { useState } from 'react'
 import BackgroundColorPickerComp from '../components/BackgroundColorPickerComp.jsx'
 import ElementColorPickerComp from '../components/ElementColorPickerComp.jsx'
+import CheckContrastComp from '../components/CheckContrastComp.jsx'
 import SubheaderComp from '../components/SubheaderComp.jsx'
 
 function SubheaderControlsComp () {
   const [bgColor, setBgColor] = useState('#ffffff')
   const [elColor, setElColor] = useState('black')
+  const bgColorToCheck = bgColor.replace('#', '')
 
   return (
     <div className="c-subheadercontrols">
       <h1>Subheader Color Controls</h1>
       <BackgroundColorPickerComp textLabel="Subheader Background Color" backgroundColor={bgColor} onBackgroundColorChange={setBgColor} />
       <ElementColorPickerComp fieldsetLabel="Subheader Element Color" onElementColorChange={setElColor} isDefault={elColor} />
+      <CheckContrastComp checkForeground={elColor === 'black' ? '000000' : 'ffffff'} checkBackground={bgColorToCheck} />
       <h2>Sample Banner</h2>
       <SubheaderComp backgroundColor={bgColor} elementColor={elColor} bannerLink={'https://escholarship.org/uc/bling_formal_linguistics'} unitTitle={'Lorem Ipsum Consectetur Adipisicing Elit'} isWide={false} campusLabel={'UC Berkeley'} campusLink={'https://escholarship.org/uc/ucb'} />
     </div>

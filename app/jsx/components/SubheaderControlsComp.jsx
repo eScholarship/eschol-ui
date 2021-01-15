e9bd42ef59178982014544c70cebd44aa97f399f
JoelCDL <joel.hagedorn@ucop.edu>
2021-01-14 16:44:19 -0800

    move subheader control ternary from prop to var

--- app/jsx/components/SubheaderControlsComp.jsx
+++ app/jsx/components/SubheaderControlsComp.jsx
@@ -10,13 +10,14 @@ function SubheaderControlsComp () {
   const [bgColor, setBgColor] = useState('#ffffff')
   const [elColor, setElColor] = useState('black')
   const bgColorToCheck = bgColor.replace('#', '')
+  const elColorToCheck = elColor === 'black' ? '000000' : 'ffffff'
 
   return (
     <div className="c-subheadercontrols">
       <h1>Subheader Color Controls</h1>
       <BackgroundColorPickerComp textLabel="Subheader Background Color" backgroundColor={bgColor} onBackgroundColorChange={setBgColor} />
       <ElementColorPickerComp fieldsetLabel="Subheader Element Color" onElementColorChange={setElColor} isDefault={elColor} />
-      <CheckContrastComp checkForeground={elColor === 'black' ? '000000' : 'ffffff'} checkBackground={bgColorToCheck} />
+      <CheckContrastComp checkForeground={elColorToCheck} checkBackground={bgColorToCheck} />
       <h2>Sample Banner</h2>
       <SubheaderComp backgroundColor={bgColor} elementColor={elColor} bannerLink={'https://escholarship.org/uc/bling_formal_linguistics'} unitTitle={'Lorem Ipsum Consectetur Adipisicing Elit'} isWide={false} campusLabel={'UC Berkeley'} campusLink={'https://escholarship.org/uc/ucb'} />
     </div>
